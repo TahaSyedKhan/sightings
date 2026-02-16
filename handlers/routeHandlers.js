78de7c9855022f317ec1125beb6b11d73eaab5e1
TahaSyedKhan <tahasyedkhan123@gmail.com>
2026-02-16 22:13:40 +0530

    added eventEmitter

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,3 +1,4 @@
+import { sightingEvents } from "../events/sightingEvents.js";
 import { addNewSighting } from "../utils/addNewSighting.js";
 import { getData } from "../utils/getData.js";
 import { getSanitizedData } from "../utils/getSanitizedData.js";
@@ -17,6 +18,7 @@ export async function handlePost(req, res) {
         const parsedBody = await parseJSONBody(req)
         const sanitizedData = getSanitizedData(parsedBody)
         await addNewSighting(sanitizedData)
+        sightingEvents.emit('sighting-added', sanitizedData)
         sendResponse(res, 201, 'application/json', JSON.stringify(sanitizedData))
     } catch (err) {
         sendResponse(res, 400, 'application/json', JSON.stringify({error: err}))
